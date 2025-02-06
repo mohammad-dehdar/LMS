b5eee0a00f88239f7b27d33da64d2e262398d1e8
mohammad dehdar <dehdardevelop@gmail.com>
2025-02-06 22:38:49 +0330

    final

--- app/(dashboard)/(routes)/search/page.js
+++ app/(dashboard)/(routes)/search/page.js
@@ -1,35 +1,46 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
+import db from "@/lib/db";
+import SearchInput from "@/components/templates/search/SearchInput";
+import { getCourses } from "@/actions/get-courses";
 import Categories from "@/components/templates/search/Categories";
 import CoursesList from "@/components/templates/search/CoursesList";
-import SearchInput from "@/components/templates/search/SearchInput";
 
-async function SearchPage({searchParams}) {
-  const {userId} = await auth();
-  if(!userId) return redirect("/");
+const SearchPage = async ({ searchParams }) => {
+  const { userId } = await auth();
+  
+  if(!userId) {
+    return redirect("/");
+  }
 
   const categories = await db.category.findMany({
     orderBy: {
       name: "asc"
     }
-  })
+  });
 
   const courses = await getCourses({
     userId,
     ...searchParams
-  })
+  });
 
   return (
     <>
       <div className="block px-6 pt-6 md:hidden md:mb-0">
         <Suspense fallback={<div>Loading...</div>}>
-          <SearchInput/>
+          <SearchInput />
         </Suspense>
       </div>
       <div className="p-6 space-y-4">
-        <Categories items={categories} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Categories items={categories} />
+        </Suspense>
         <CoursesList items={courses} />
       </div>
     </>
-  )
-}
+  );
+};
+
+export default SearchPage;
